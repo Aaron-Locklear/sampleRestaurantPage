dd2748ac34a42890253898b28a5f328f23604e4d
Aaron Locklear <aaronbenjaminlocklear@gmail.com>
2023-08-17 01:58:36 -0600

    Add js file to control DOM manipulation

---
+++ src/pageControl.js
@@ -0,0 +1,14 @@
+const pageControl = (function(){
+  function loadBase(){
+    const header = document.createElement("div");
+    const home = document.createElement("div");
+    const menu = document.createElement("div");
+    const contact = document.createElement("div");
+
+    home.innerHtml = ()
+  }
+
+  return {
+    loadBase,
+  }
+})();
